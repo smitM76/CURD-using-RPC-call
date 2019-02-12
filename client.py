eabbf9e5956b7fc5d6496ee065d4318fffc19938
smitM76 <smitmodh99@gmail.com>
2019-02-12 13:05:54 +0530

    added explanation of code client.py

--- client.py
+++ client.py
@@ -1,3 +1,8 @@
+"""This module works as client, make rpc call to server.py
+   this client can request for GET,POST,PUT,DELETE
+   made use of argparser make it easy to understad input or uses
+   -h [client.py -h] for help
+"""
 import pika
 import uuid
 import json
@@ -5,9 +10,24 @@ import logging
 from logging.config import dictConfig
 import argparse
 
+"""for externel rabbitmq service
+url = 'amqp://dszgrpet:Yw5yKoMZqINvp78V63Su7Svw02KyhCcX@dinosaur.rmq.cloudamqp.com/dszgrpet'
+params = pika.URLParameters(url)
+"""
+
 
 class Rpc_client(object):
+    """establish connection to rabbitmq running locally
+       refere rabbitmq documentation for more details 
+    """
+    """for externel rabbitmq service
+       url = 'url'  # I have used https://www.cloudamqp.com/
+       params = pika.URLParameters(url)
+       # self.connection = pika.BlockingConnection(params) insted of 
+       # self.connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
+    """
     def __init__(self):
+        #self.connection = pika.BlockingConnection(params) 
         self.connection = pika.BlockingConnection(
             pika.ConnectionParameters(host='localhost'))
         self.channel = self.connection.channel()
@@ -21,6 +41,8 @@ class Rpc_client(object):
         if self.corr_id == props.correlation_id:
             self.response = body
 
+    """this is the same method you found on rabbitmq rpc tutorial
+    """
     def call(self, data):
         self.response = None
         self.corr_id = str(uuid.uuid4())
@@ -30,17 +52,21 @@ class Rpc_client(object):
                                        reply_to=self.callback_queue,
                                        correlation_id=self.corr_id
                                    ),
-                                   body=str(data))
+                                   body=str(data)) #user input goes here
         while self.response is None:
             self.connection.process_data_events()
         return self.response
 
+#initialising argparser with basic operations
 
 parser = argparse.ArgumentParser()
 parser.add_argument('method', help='Select The Method',
                     choices=["GET", "POST", "PUT", "DELETE"])
 
+# handles sub arguments
 args, sub_args = parser.parse_known_args()
+
+# setting values as message based on user input
 if args.method == 'GET':
     msg = {
         "method": "GET"
@@ -48,12 +74,19 @@ if args.method == 'GET':
 
 elif args.method == 'POST':
     parser = argparse.ArgumentParser()
+    """adding following arguments runtime or when POST is input,
+     to avoid complexity and the confusion of add argument with main options,
+     these both are compulsory argument,
+     preventing user to input null values 'LOL'
+    """
     parser.add_argument('username', help='username', type=str)
     parser.add_argument('comment', help='comment', type=str)
 
     args = parser.parse_args(sub_args)
     u_name = args.username
     u_comment = args.comment
+
+    # setting the payload with user input
     msg = {
         "method": "POST",
         "username": u_name,
@@ -62,6 +95,12 @@ elif args.method == 'POST':
 
 elif args.method == 'PUT':
     parser = argparse.ArgumentParser()
+    """adding following arguments runtime or when PUT is input,
+     to avoid complexity and the confusion of add argument with main options,
+     making id as compulsory argument and --username and --comment as optional
+     arguments
+    """
+
     parser.add_argument('id', help='id', type=int)
     parser.add_argument('--username', help='username', type=str)
     parser.add_argument('--comment', help='comment', type=str)
@@ -70,6 +109,8 @@ elif args.method == 'PUT':
     u_id = args.id
     u_name = args.username
     u_comment = args.comment
+
+    # setting the payload with user input
     msg = {
         "method": "PUT",
         "id": u_id,
@@ -78,10 +119,14 @@ elif args.method == 'PUT':
     }
 
 elif args.method == "DELETE":
+    """adding compulsory sub argument id
+    """
     parser = argparse.ArgumentParser()
     parser.add_argument('id', help='id', type=int)
     args = parser.parse_args(sub_args)
     u_id = args.id
+
+    # setting the payload with user input
     msg = {
         "method": "DELETE",
         "id": u_id
@@ -91,5 +136,6 @@ else:
 
 rpc = Rpc_client()
 print(" [x] Requesting ")
+# passing the user input as json
 response = rpc.call(json.dumps(msg))
 print(" [.] Got %r" % response)

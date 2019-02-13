fdcd37a6c01659af1b26801b728049345d4360cc
smitM76 <smitmodh99@gmail.com>
2019-02-13 18:08:01 +0530

    added code explaination to server.py

--- server.py
+++ server.py
@@ -1,3 +1,7 @@
+"""server implementation,
+    listen for rpc call and return the response as per request
+    refere rabbitmq rpc documentation for basic server implementation
+"""
 import pika
 import queries as Q
 import json
@@ -8,6 +12,12 @@ logging.config.dictConfig(config)
 
 logger = logging.getLogger(__name__)
 
+"""for externel rabbitmq service
+       url = 'url'  # I have used https://www.cloudamqp.com/
+       params = pika.URLParameters(url)
+       # self.connection = pika.BlockingConnection(params) insted of
+       # self.connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
+"""
 connection = pika.BlockingConnection(
     pika.ConnectionParameters(host='localhost'))
 
@@ -17,12 +27,15 @@ channel.queue_declare(queue='rpc_queue')
 
 
 def on_request(ch, method, props, body):
-
+    """loading data recieved from rpc client
+    """
     data = json.loads(str(body)[2:-1])
     print('Recieved -> {}'.format(data))
 
+    # Display what request we got
     logger.info('Recieved -> {}'.format(data))
 
+    # Inthis section as per client request calling the coresponding method which return desired output
     if data['method'] == 'GET':
         response = Q.display_data()
     elif data['method'] == 'POST':
@@ -37,6 +50,7 @@ def on_request(ch, method, props, body):
     else:
         response = "wrong api"
 
+    # sending response back to client
     ch.basic_publish(
         exchange='',
         routing_key=props.reply_to,
@@ -45,7 +59,7 @@ def on_request(ch, method, props, body):
     )
     ch.basic_ack(delivery_tag=method.delivery_tag)
 
-
+# works like loop keep listning for requestss
 channel.basic_consume(on_request, queue='rpc_queue')
 print(" [x] Awaiting RPC requests")
 channel.start_consuming()
